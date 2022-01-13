90859aa0d4cd1c45f7813e51e0f83899358566ae
TNolan01 <ihavesevenfingers@gmail.com>
2022-01-13 22:01:24 +0000

    Work on rain_chart.hml to create graph

--- js/rainChart.js
+++ js/rainChart.js
@@ -3,18 +3,128 @@ async function rainChart() {
     const data = await response.json();
     console.log(data);
 
-    console.log(data.summaries[6].metric.precipTotal);
-  /*   summaries[0].obsTimeLocal
-    
-    summaries[0].metric.precipTotal */
-  
-    var pressure = data.observations[0].metric.pressure;
-
-    summaries[1].obsTimeUtc
-    
-  
-
-    console.log(res);
-
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
-rainChart();
+
+
+
+
+
+
+
+
+
+/* 
+  var chartObject = JSON.parse(chart)
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+    type: "horizontalBar",
+    data: {
+      labels: [
+        chartObject[0][0],
+        chartObject[0][1],
+        chartObject[0][2],
+        chartObject[0][3],
+        chartObject[0][4],
+        chartObject[0][5],
+        chartObject[0][6],
+        chartObject[0][7],
+        chartObject[0][8],
+        chartObject[0][9]
+      ],
+      datasets: [
+        {
+          label: "Frequency",
+          data: [
+            chartObject[1][0],
+            chartObject[1][1],
+            chartObject[1][2],
+            chartObject[1][3],
+            chartObject[1][4],
+            chartObject[1][5],
+            chartObject[1][6],
+            chartObject[1][7],
+            chartObject[1][8],
+            chartObject[1][9]
+          ],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)"
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)"
+          ],
+          borderWidth: 2
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+    },
+    title: {
+            display: true,
+            text: 'Test Count'
+
+    }
+  }
+}
+
+ */
