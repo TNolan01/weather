c3ee7effec6253bbd723c1abf2958a0cc2faba7c
TNolan01 <ihavesevenfingers@gmail.com>
2022-01-14 00:47:45 +0000

    work on rainchart.js and rain_chart.html to get data into graph

--- js/rainChart.js
+++ js/rainChart.js
@@ -1,10 +1,11 @@
 async function rainChart() {
     const response = await fetch('https://api.weather.com/v2/pws/dailysummary/7day?stationId=IENNIS18&format=json&units=m&apiKey=d26d907038e74f7fad907038e7ef7f0e');
     const data = await response.json();
+    console.log(data);
     let day_name=[]; /* new array to hold day name for chart */
     /* convert date and time into a day referecnce for chart label */
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      for (i=0; i<6; i++) {
+      for (i=0; i<7; i++) {
         console.log(data.summaries[i].obsTimeLocal);
         let date = data.summaries[i].obsTimeLocal;  
         let today= days[new Date(date).getDay()];
@@ -15,16 +16,24 @@ async function rainChart() {
     const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: [data.summaries[0].obsTimeLocal.split(' ')[0].trim(), 
-                    data.summaries[1].obsTimeLocal.split(' ')[0].trim(),
+            labels: [day_name[0], 
+                    day_name[1],
                     day_name[2], 
-                    'Green', 
-                    'Purple', 
-                    'Orange'
+                    day_name[3], 
+                    day_name[4], 
+                    day_name[5],
+                    day_name[6]
                   ],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: 'Rainfall over last 7days',
+                data: [data.summaries[0].metric.precipTotal,
+                      data.summaries[1].metric.precipTotal,
+                      data.summaries[2].metric.precipTotal,
+                      data.summaries[3].metric.precipTotal,
+                      data.summaries[4].metric.precipTotal,
+                      data.summaries[5].metric.precipTotal,
+                      data.summaries[6].metric.precipTotal
+                      ],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
